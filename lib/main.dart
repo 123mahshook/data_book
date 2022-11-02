e74c17f419acb10fe557604ea8c0928514c0cbe3
123mahshook <37013247+123mahshook@users.noreply.github.com>
2022-11-02 13:01:21 +0530

    stream builder example

--- lib/main.dart
+++ lib/main.dart
@@ -169,3 +169,76 @@ class Home {
     return address;
   }
 } */
+
+/* //
+//
+// Stream Provider Example
+//
+//
+
+void main() {
+  runApp(
+    StreamProvider<String>(
+      create: (_) => Person(name: 'Jhone Doe', initialAge: 25).age,
+      initialData: 25.toString(),
+      catchError: (_, error) => error.toString(),
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stream Provider"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Consumer<String>(
+            builder: (context, String age, child) {
+              return Column(
+                children: <Widget>[
+                  Text("Watch Jhone Age..."),
+                  Text("name: Jhone Doe"),
+                  Text("age: $age"),
+                ],
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Person {
+  Person({this.name, this.initialAge});
+
+  final String? name;
+  final int? initialAge;
+
+  Stream<String> get age async* {
+    var i = initialAge!;
+    while (i < 85) {
+      await Future.delayed(Duration(seconds: 1), () {
+        i++;
+      });
+      yield i.toString();
+    }
+  }
+}
+ */
